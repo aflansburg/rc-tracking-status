c4f5de52498dba7458847e6475fbe8e4de19711f
Abram Flansburg <abeflansburg@gmail.com>
2018-05-08 17:00:30 -0500

    need to figure out wsdl file read

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fedexBatchTrack from './helpers/fedexBatchTracking';
+import testTracking from './helpers/data/sample_tracking.json';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      results: null,
+      trackingNo: testTracking.tracking,
+    }
+
+    this.fetchTracking = this.fetchTracking.bind(this);
+  }
+
+  fetchTracking(event){
+    fedexBatchTrack(this.state.trackingNo)
+      .then(data=>{
+        this.setState = {results: data}
+      })
+  }
+  
+  componentDidMount(){
+
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Fedex Tracking Status</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="interactions">
+          <Button 
+            onClick={() => this.fetchTracking()}
+            className="fetch-btn"
+          >
+            Get Tracking
+          </Button>
+        </div>
+        <div>
+          { this.state.results
+            ? <p>{this.state.results}</p>
+            : null
+          }
+        </div>
       </div>
     );
   }
 }
+
+class Table extends Component {
+
+  render(){
+    return(
+      <div className="results-table">
+
+      </div>
+    )
+  }
+}
+
+const Button = ({ onClick, className, children }) => 
+  <button
+    onClick={onClick}
+    className={className}
+    type="button">
+    {children}
+  </button>
+  
 
 export default App;
