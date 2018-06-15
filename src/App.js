cc63effcac525a8f2e1581cd654f45e669295dc0
Abram Flansburg <abeflansburg@gmail.com>
2018-06-15 15:55:30 -0500

    web app is up and running

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactTable from "react-table";
+import moment from 'moment';
+import 'react-table/react-table.css';
 import getTracking from './helpers/getTracking';
-import testTracking from './helpers/data/sample_tracking.json';
+import {CSVLink} from 'react-csv';
+import logo from './rc-logo.png';
+
 
 class App extends Component {
   constructor(props){
@@ -9,18 +14,19 @@ class App extends Component {
 
     this.state = {
       results: null,
-      trackingNo: testTracking.tracking,
+      btnClicked: false,
     }
 
     this.fetchTracking = this.fetchTracking.bind(this);
   }
 
   fetchTracking(event){
+    this.setState({btnClicked: true});
     getTracking.get()
       .then(data=>{
-        console.log(`data returned: ${data}`);
+        console.dir(data);
         this.setState({results: JSON.parse(data)});
-        console.log('state should be updated');
+        // console.log('state should be updated');
       })
       .catch(err=>{console.log(err);})
   }
@@ -28,49 +34,100 @@ class App extends Component {
   render() {
 
     const list = this.state.results;
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Fedex Tracking Status</h1>
+          <img src={logo} alt="Rough Country Logo"/>
+          <h1 className="App-title">Tracking Status</h1>
         </header>
         <div className="interactions">
           <Button 
             onClick={() => this.fetchTracking()}
-            className="fetch-btn"
+            // className="fetch-btn pulse"
+            className={this.state.btnClicked ? 'fetch-btn' : 'fetch-btn pulse' }
           >
             Get Tracking
           </Button>
+          <div className="post-interactions">
+          {list
+          // need to set to download filtered list
+            ? <Button className="fetch-btn csvlink-btn pulse-after">
+                <CSVLink className="csvlink" filename={"tracking_data.csv"} data={list} target="_blank">
+                  Download CSV
+                </CSVLink>
+              </Button>
+            : null }
+          </div>
         </div>
-        {list
-         ? <Table 
-            list={list}/>
-         : <p>No Data To Display</p>}
+        
+         {list
+          ? <ReactTable
+          className='-striped -highlight'
+          data={list}
+          columns={columns}
+          filterable
+          />
+          : null
+         }
       </div>
     );
   }
 }
 
-const Table = ({ list }) =>
-  <div className="results-table">
-    <table>
-      <tbody>
-        <tr>
-          <th>Number</th>
-          <th>Status</th>
-        </tr>
-        {list.map(item=>{
-          <tr>
-            <td>
-              {item.trackingNum}
-            </td>
-            <td>
-              {item.lastStatus}
-            </td>
-          </tr>
-        })}
-      </tbody>
-    </table>
-  </div>
+const columns = [{
+  Header: 'Tracking No.',
+  accessor: 'trackingNum',
+}, {
+  Header: 'Status',
+  id: "lastStatus",
+  accessor: d => d.lastStatus,
+  Cell: row => (
+    <div style={{
+      color:
+        row.value && ["delivery exception", "shipment exception"].includes(row.value.toLowerCase())
+        ? "#ff3721" 
+        : row.value && row.value.toLowerCase() === "shipment information sent to fedex" ? "#4286f4" : "#1ebc09",
+    }}>{row.value}</div>
+  ),
+  filterMethod: (filter, row) => {
+    if (filter.value === "all"){
+      return true;
+    }
+    if (filter.value === "unscanned"){
+      if (row[filter.id])
+        return row[filter.id].toLowerCase() === "shipment information sent to fedex";
+    }
+    if (filter.value === "exception"){
+      if (row[filter.id])
+        return row[filter.id].toLowerCase() === "delivery exception" || row[filter.id].toLowerCase() === 'shipment exception';
+    }
+  },
+  Filter: ({ filter, onChange }) =>
+    <select
+      onChange={event => onChange(event.target.value)}
+      style={{ width: "100%"}}
+      value={filter ? filter.value : "all"}
+    >
+      <option value="all">Show All</option>
+      <option value="unscanned">Unscanned</option>
+      <option value="exception">Exception</option>
+    </select>
+}, {
+  id: 'shipDate',
+  Header: 'Ship Date',
+  accessor: d => {
+    if (d.shipDate)
+      return moment(d.shipDate)
+        .local()
+        .format("MM-DD-YYYY");
+  }, 
+}, {
+  id: 'reason',
+  Header: 'Reason',
+  accessor: 'reason',
+  // TO DO: add filter for reason
+}]
 
 const Button = ({ onClick, className, children }) => 
   <button
--- src/helpers/getTracking.js
+++ src/helpers/getTracking.js
@@ -1,5 +1,6 @@
 const request = require('request-promise-native');
-const endpoint = 'http://localhost:3000/tracking';
+// this is the endpoint for the REST API
+const endpoint = 'http://192.168.1.251:3000/tracking';
 
 const get = () =>{
     return request.get(endpoint);
